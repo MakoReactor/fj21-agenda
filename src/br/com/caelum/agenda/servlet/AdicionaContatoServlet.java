5645342c9ba92df0c38a4c213e45877e11f8a2ef
Douglas Barbosa dos Santos <douglas.makubex@gmail.com>
2014-12-03 17:44:36 -0200

    capitulo 5 da apostila finalizado

--- src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -16,6 +16,11 @@ import br.com.caelum.agenda.dao.ContatoDao;
 import br.com.caelum.agenda.modelo.Contato;
 
 public class AdicionaContatoServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
